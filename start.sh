73e12d46ac287c0e1dbbdc1564f2dc14d4e939a2
Robert Dunmire III <slackr31337@gmail.com>
2018-01-09 17:02:46 -0500

    First Commit

---
+++ start.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+cd /var/www/html
+wget https://core-releases.s3-us-west-2.amazonaws.com/6615/1380/8304/concrete5-8.3.1.zip
+unzip concrete5-8.3.1.zip
+rm -rf concrete5-8.3.1.zip
+
+/usr/bin/supervisord -n -c /etc/supervisor/supervisord.conf
+
