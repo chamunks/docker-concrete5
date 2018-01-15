fa8a775da33a69761d2405ec7f2e3aada5171b42
Robert Dunmire III <slackr31337@gmail.com>
2018-01-15 16:23:41 -0500

    Update start.sh

--- start.sh
+++ start.sh
@@ -5,11 +5,10 @@ fi
 cd /var/www/
 unzip -n -q /var/www/concrete5.zip
 mv /var/www/concrete5*/* /var/www/html/
-chown -R www-data:www-data /var/www/html/*
 rm -rf /var/www/concrete5*
 rm -rf /var/www/html/index.html
 rm -rf /var/www/html/updates
-chown -R www-data:www-data /var/www/html/files
+chown -R www-data:www-data /var/www/html/*
 chmod 777 /var/www/html/files
 
 /usr/bin/supervisord -n -c /etc/supervisor/supervisord.conf
