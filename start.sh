d7d81066d1afb1a97908cdd814d474565650d570
Robert Dunmire III <slackr31337@gmail.com>
2018-01-15 16:07:45 -0500

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,14 +1,13 @@
 #!/bin/sh
-if [ -z "$URL" ]; then
-URL="https://core-releases.s3-us-west-2.amazonaws.com/6615/1380/8304/concrete5-8.3.1.zip"
-fi
 cd /tmp
-echo "<H1><center>Loading i-Content....</center></H1>" > /var/www/html/index.html
-wget -q $URL -O /tmp/concrete5.zip
-unzip -n -q concrete5.zip
+unzip -n -q /tmp/concrete5.zip
 mv /tmp/concrete5-*/* /var/www/html/
 chown -R www-data:www-data /var/www/html/*
 rm -rf /tmp/concrete5*
 rm -rf /var/www/html/index.html
+rm -rf /var/www/html/updates
+chown -R www-data:www-data /var/www/html/files
+chmod 777 /var/www/html/files
+
 /usr/bin/supervisord -n -c /etc/supervisor/supervisord.conf
 
