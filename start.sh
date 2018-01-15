6b4a5384659e4b0569755af7dfd05d9086243d9d
Robert Dunmire III <slackr31337@gmail.com>
2018-01-15 16:21:34 -0500

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,4 +1,8 @@
 #!/bin/sh
+if [ ! -f /var/www/concrete5.zip ];then
+wget -q https://core-releases.s3-us-west-2.amazonaws.com/6615/1380/8304/concrete5-8.3.1.zip -O /var/www/concrete5.zip
+fi
+cd /var/www/
 unzip -n -q /var/www/concrete5.zip
 mv /var/www/concrete5*/* /var/www/html/
 chown -R www-data:www-data /var/www/html/*
