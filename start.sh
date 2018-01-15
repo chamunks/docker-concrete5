75cb0ec71285e1ad0783da8e99385a077b654b96
Robert Dunmire III <slackr31337@gmail.com>
2018-01-15 15:16:08 -0500

    Update start.sh

--- start.sh
+++ start.sh
@@ -2,9 +2,7 @@
 cd /var/www/html
 wget https://core-releases.s3-us-west-2.amazonaws.com/6615/1380/8304/concrete5-8.3.1.zip
 unzip concrete5-8.3.1.zip
-rm -rf concrete5-8.3.1.zip
-cd concrete5-8.3.1/
-mv * ../
-
+mv concrete5-8.3.1/* /var/www/html/
+rm -rf concrete5-8.3.1*
 /usr/bin/supervisord -n -c /etc/supervisor/supervisord.conf
 
