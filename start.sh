6343a447987ada6cff433ccb8c69ef7ad50e3001
Robert Dunmire III <slackr31337@gmail.com>
2018-01-16 13:10:41 -0500

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,19 +1,14 @@
 #!/bin/sh
 if [ ! -f /var/www/concrete5.zip ];then
-	wget -q https://core-releases.s3-us-west-2.amazonaws.com/6615/1380/8304/concrete5-8.3.1.zip -O /var/www/concrete5.zip
+	wget -q https://www.concrete5.org/download_file/-/view/96083/8497/ -O /var/www/concrete5.zip
 fi
 cd /var/www/
 unzip -n -q /var/www/concrete5.zip
-mv /var/www/concrete5*/* /var/www/html/
-mv /var/www/concrete5*/composer.* /var/www/html/
-mv /var/www/concrete5*/index.php /var/www/html/
-mv /var/www/concrete5*/concrete /var/www/html/
-mv /var/www/concrete5*/application /var/www/html/application-orig
-if [ ! -d /var/www/html/application/config ] ;then
-	cp -rf /var/www/html/application-orig/* /var/www/html/application/
-fi
-if [ ! -d /var/www/html/packages ];then
-	mkdir var/www/html/packages
+if [ ! -d /var/www/html/config ];then
+	cp -nr /var/www/concrete5*/* /var/www/html/
+else
+	cp -rf /var/www/concrete5*/index.php /var/www/html/
+	cp -rf /var/www/concrete5*/concrete /var/www/html/
 fi
 rm -rf /var/www/concrete5*
 rm -rf /var/www/html/updates
