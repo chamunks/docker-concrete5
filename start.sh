4701b8b41123742bfcad7f81352d9130db517ed2
Robert Dunmire III <slackr31337@gmail.com>
2018-01-16 09:22:51 -0500

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,16 +1,18 @@
 #!/bin/sh
-if [ ! -f /var/www/concrete5.zip ];then
-wget -q https://core-releases.s3-us-west-2.amazonaws.com/6615/1380/8304/concrete5-8.3.1.zip -O /var/www/concrete5.zip
-fi
 cd /var/www/
-unzip -n -q /var/www/concrete5.zip
-mv /var/www/concrete5*/* /var/www/html/
-
-rm -rf /var/www/concrete5*
+if [ ! -f /var/www/html/application/config/database.php ] ;then
+	if [ ! -f /var/www/concrete5.zip ] ;then
+		wget -q https://core-releases.s3-us-west-2.amazonaws.com/6615/1380/8304/concrete5-8.3.1.zip -O /var/www/concrete5.zip
+	fi
+	unzip -n -q /var/www/concrete5.zip
+	mv /var/www/concrete5*/* /var/www/html/
+	rm -rf /var/www/concrete5*
+	rm -rf /var/www/html/updates
+fi
 rm -rf /var/www/html/index*.html
-
 chown -R www-data:www-data /var/www/html/*
 chmod 777 /var/www/html/files
 
 /usr/bin/supervisord -n -c /etc/supervisor/supervisord.conf
+
 
