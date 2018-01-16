5e39d5b48ae6b2b185ef35a3699f27f117aeaf12
Robert Dunmire III <slackr31337@gmail.com>
2018-01-16 14:50:36 -0500

    Update start.sh

--- start.sh
+++ start.sh
@@ -2,7 +2,7 @@
 rm -rf /var/www/html/index*.html > /dev/null 2>&1
 
 if [ ! -d /var/www/html/application/config ] ;then
-	cp -rf /var/www/html/application-orig/* /var/www/html/application/
+	cp -rf /var/www/html/application-dist/* /var/www/html/application/
 fi
 
 chown -R www-data:www-data /var/www/html/application/files*
