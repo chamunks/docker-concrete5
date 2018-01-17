eafa6e4382f5ad6680fc166b9f70ae140d473d9f
Robert Dunmire III <slackr31337@gmail.com>
2018-01-17 12:08:25 -0500

    Update start.sh

--- start.sh
+++ start.sh
@@ -5,8 +5,9 @@ if [ ! -d /var/www/html/application/config ] ;then
 	cp -rf /var/www/html/application-dist/* /var/www/html/application/
 fi
 
-chown -R www-data:www-data /var/www/html/application/files*
-chmod -R 777 /var/www/html/application/files
+chown -R www-data:www-data /var/www/html/application
+chown -R www-data:www-data /var/www/html/packages
+chmod 775 /var/www/html/application/files
 
 /usr/bin/supervisord -n -c /etc/supervisor/supervisord.conf
 
