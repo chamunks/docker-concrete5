e1f5f07320f3575f92388e0f92b472421d9d98d8
Robert Dunmire III <slackr31337@gmail.com>
2018-01-18 15:08:14 -0500

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,5 +1,4 @@
 #!/bin/sh
-mkdir /run/haproxy
 
 if [ ! -d /var/www/html/config ];then
 	cp -nr /var/www/concrete5.6.3.5/* /var/www/html/
@@ -8,8 +7,8 @@ rm -rf /var/www/html/index*.html > /dev/null 2>&1
 chown -R www-data:www-data /var/www/html/*
 chmod 775 /var/www/html/files
 
-mkdir /var/www/html/updates
-ln -s /var/www/html /var/www/html/updates/concrete5.6.3.5
+mkdir /var/www/html/updates || rm -rf /var/www/html/updates/*
+ln -s /var/www/html /var/www/html/updates/concrete5.6.3.5 
 
 /usr/bin/supervisord -n -c /etc/supervisor/supervisord.conf
 
