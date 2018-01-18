2078294e8e7b61055a97b30445fe7b6aa186618c
Robert Dunmire III <slackr31337@gmail.com>
2018-01-18 15:32:15 -0500

    Update start.sh

--- start.sh
+++ start.sh
@@ -9,7 +9,7 @@ chown -R www-data:www-data /var/www/html/application
 chown -R www-data:www-data /var/www/html/packages
 chmod 775 /var/www/html/application/files
 
-mkdir /var/www/html/updates
+mkdir /var/www/html/updates 2> /dev/null || rm -rf /var/www/html/updates/*
 ln -s /var/www/html /var/www/html/updates/concrete5-8.3.1
 
 /usr/bin/supervisord -n -c /etc/supervisor/supervisord.conf
