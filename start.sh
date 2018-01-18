dd47bd44a4680d40b1349ade3adb20c93014e083
Robert Dunmire III <slackr31337@gmail.com>
2018-01-18 15:31:43 -0500

    Update start.sh

--- start.sh
+++ start.sh
@@ -7,7 +7,7 @@ rm -rf /var/www/html/index*.html > /dev/null 2>&1
 chown -R www-data:www-data /var/www/html/*
 chmod 775 /var/www/html/files
 
-mkdir /var/www/html/updates || rm -rf /var/www/html/updates/*
+mkdir /var/www/html/updates 2> /dev/null|| rm -rf /var/www/html/updates/*
 ln -s /var/www/html /var/www/html/updates/concrete5.6.3.5 
 
 /usr/bin/supervisord -n -c /etc/supervisor/supervisord.conf
