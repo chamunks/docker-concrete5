45988eac120572e3a82508dd276a967cda5a3dab
Robert Dunmire III <slackr31337@gmail.com>
2018-01-18 11:53:32 -0500

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,17 +1,8 @@
 #!/bin/sh
-if [ ! -f /var/www/concrete5.zip ];then
-	wget -q https://www.concrete5.org/download_file/-/view/96083/8497/ -O /var/www/concrete5.zip
-fi
-cd /var/www/
-unzip -n -q /var/www/concrete5.zip
+
 if [ ! -d /var/www/html/config ];then
 	cp -nr /var/www/concrete5*/* /var/www/html/
-else
-	cp -rf /var/www/concrete5*/index.php /var/www/html/
-	cp -rf /var/www/concrete5*/concrete /var/www/html/
 fi
-rm -rf /var/www/concrete5*
-rm -rf /var/www/html/updates
 rm -rf /var/www/html/index*.html > /dev/null 2>&1
 chown -R www-data:www-data /var/www/html/*
 chmod -R 777 /var/www/html/files
