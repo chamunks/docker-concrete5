8ab5f05344c5b3b6071f19c3b809b3d893d087cc
Robert Dunmire III <slackr31337@gmail.com>
2018-01-26 11:14:45 -0500

    Update concrete.php

--- concrete.php
+++ concrete.php
@@ -1,5 +1,15 @@
 <?php
+
 return array(
+    'marketplace' => array(
+        'enabled' => false,
+        'intelligent_search' => false,
+    ),
+    'external' => array(
+        'intelligent_search_help' => false,
+        'news_overlay' => false,
+        'news' => false,
+    ),
     'email' => array(
         'default' => array(
             'address' => 'no-reply@cms.i-evolve.net',
