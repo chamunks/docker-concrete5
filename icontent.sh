9f527fc0be225738db1c707e239f321453613428
Robert Dunmire III <slackr31337@gmail.com>
2018-02-05 13:00:46 -0500

    Update icontent.sh

--- icontent.sh
+++ icontent.sh
@@ -27,7 +27,7 @@ else
     if [ "$CMS_VER" != "$CMS_INSTALLED" ];then
         echo "Upgrading C5 from ${CMS_INSTALLED} to ${CMS_VER}... " > /dev/stdout
         chown -R www-data:www-data /var/www/html/application
-        /var/www/html/concrete/bin/concrete5 -f c5:update > /dev/stdout
+        /var/www/html/concrete/bin/concrete5 -f c5:update 
         chown -R www-data:www-data /var/www/html/application
     fi
     mkdir /var/www/sites 2> /dev/null || rm -rf /var/www/sites/*
